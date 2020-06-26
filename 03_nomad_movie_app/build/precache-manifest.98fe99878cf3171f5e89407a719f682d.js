415090d4a6ed833b52f0bd6b0341ad1f8e31429e
jumpupmoon <jumpupmoon@naver.com>
2020-06-26 15:20:26 +0900

    build

---
+++ 03_nomad_movie_app/build/precache-manifest.98fe99878cf3171f5e89407a719f682d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6db36fdbbdf080ae35a90f0203b5d614",
+    "url": "/js-framework/index.html"
+  },
+  {
+    "revision": "6752a2ab01bd9a99ca43",
+    "url": "/js-framework/static/css/main.15bc49a7.chunk.css"
+  },
+  {
+    "revision": "3fedcf50dc8357f08e49",
+    "url": "/js-framework/static/js/2.68c5b8b0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/js-framework/static/js/2.68c5b8b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6752a2ab01bd9a99ca43",
+    "url": "/js-framework/static/js/main.ff6ea2d1.chunk.js"
+  },
+  {
+    "revision": "63f3c6dea6e08c3a5495",
+    "url": "/js-framework/static/js/runtime-main.7f4c4b47.js"
+  }
+]);
